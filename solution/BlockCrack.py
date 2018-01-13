b3c1e32a6bb33481aa5874279eab70e7bba9f9fd
JacksonRussett <35400148+JacksonRussett@users.noreply.github.com>
2018-01-13 16:28:21 -0500

    Add files via upload

--- solution/BlockCrack.py
+++ solution/BlockCrack.py
@@ -1,24 +1,21 @@
 from random import *
 import Part1
 
-def Crack(ciphertext, k=None):
+def CrackKnown(ciphertext, k):
     ceiling = 1000
     
-    if(k == None):
-        return
-    else:
-        count = 0
-        kp = randint(0, pow(10, k)-1)
-        prev = Part1.BlockDecrypt(kp, ciphertext)
+    count = 0
+    kp = randint(0, pow(10, k)-1)
+    prev = Part1.BlockDecrypt(kp, ciphertext)
+    kn = randint(0, pow(10, k)-1)
+    next = Part1.BlockDecrypt(kn, ciphertext)
+    while count<ceiling:
+        if(Score(prev)<Score(next)):
+            kp = kn
+            prev = next
+            count = 0
         kn = randint(0, pow(10, k)-1)
         next = Part1.BlockDecrypt(kn, ciphertext)
-        while count<ceiling:
-            if(Score(prev)<Score(next)):
-                kp = kn
-                prev = next
-                count = 0
-            kn = randint(0, pow(10, k)-1)
-            next = Part1.BlockDecrypt(kn, ciphertext)
-            count += 1
-        
-        return [kp, prev]
+        count += 1
+    
+    return [kp, prev]
