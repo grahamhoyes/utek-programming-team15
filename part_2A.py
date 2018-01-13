4497662c6050503552779679a79c20f935d430f9
Danial Hasan <danial.s.hasan@gmail.com>
2018-01-13 14:54:28 -0500

    Add files via upload

---
+++ part_2A.py
@@ -0,0 +1,61 @@
+def get_count(dataset):
+    
+    count_dict = {}
+    
+    for i in range(len(dataset)):
+        n_gram = ""
+        end = i + 7
+        if end >= len(dataset):
+            end = len(dataset)
+        
+        j = i        
+        while j < end:
+            
+            if dataset[j] == " ":
+                end += 1
+                if end >= len(dataset):
+                    end = len(dataset)
+                j += 1
+                continue
+            else:
+                n_gram += dataset[j]
+            
+
+            if not count_dict.__contains__(n_gram):
+                count_dict[n_gram] = 1
+            else:
+                count_dict[n_gram] += 1 
+            
+            j += 1
+
+    
+    
+    return count_dict
+
+def parse_input(input):
+    clean = ""
+    for i in range(len(input)):
+        if ord(input[i]) < 65 or ord(input[i]) > 90:
+            continue
+        else:
+            clean += input[i]
+    return clean
+
+
+if  __name__ == "__main__":
+    with open("ptb.train.txt") as file:  
+        data = file.read() 
+    
+    #data = "HELLO WORLD"
+    
+    clean_input = parse_input("THE")
+
+    counts = get_count(data)
+    
+    clean_input = "the"
+    
+    print(clean_input)
+    if counts.__contains__(clean_input):
+        print(counts[clean_input])
+    else:
+        print("0")
