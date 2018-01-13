756b35fe3f1489ceb6767364257814a5d33fbeef
Graham Hoyes <grahamhoyes@gmail.com>
2018-01-13 14:25:14 -0500

    Updated main.py, created run.sh

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-import Part1
+from solution import parser, Part1
 
-k = [2, 5, 1, 3]
-text = "Hello World!"
-print(text)
+outputdir = 'output'
 
-ciphertext = Part1.BlockEncrypt(k, text)
-print(ciphertext)
+part1data = parser.parse1('input')
 
-ass = Part1.PermutationEncrypt('QWERTYUIOPASDFGHJKLZXCVBNM', 'ADASQDBWUQDJQWEOS!!!D')
-print(ass)
-ass = Part1.PermutationDecrypt('QWERTYUIOPASDFGHJKLZXCVBNM', ass)
-print(ass)
+## Part 1a
+with open(outputdir + '/1a.out', 'a') as out1a:
+    for e in part1data['1a']:
+        if e[0] == 'ENCRYPT':
+            out1a.write(Part1.SimpleEncrypt(e[1], e[2]) + '\n')
+        elif e[0] == 'DECRYPT':
+            out1a.write(Part1.SimpleDecrypt(e[1], e[2]) + '\n')
+
+## Part 1b
+with open(outputdir + '/1b.out', 'a') as out1b:
+    for e in part1data['1b']:
+        if e[0] == 'ENCRYPT':
+            out1b.write(Part1.BlockEncrypt(e[1], e[2]) + '\n')
+        elif e[0] == 'DECRYPT':
+            out1b.write(Part1.BlockDecrypt(e[1], e[2]) + '\n')
+            
+## Part 1c
+with open(outputdir + '/1c.out', 'a') as out1c:
+    for e in part1data['1c']:
+        if e[0] == 'ENCRYPT':
+            out1c.write(Part1.PermutationEncrypt(e[1], e[2]) + '\n')
+        elif e[0] == 'DECRYPT':
+            out1c.write(Part1.PermutationDecrypt(e[1], e[2]) + '\n')
+            
+## Dataset for Part 2
+parser.parse2('ptb.train.txt')
--- solution/parser.py
+++ solution/parser.py
@@ -5,45 +5,38 @@ Author: Graham Hoyes
 Description: File parser
 """
 
-import os
-import re
+def parse1(inputdir):    
+    res = {'1a': [], '1b': [], '1c':[]}
+    
+    files1 = ['1a.in', '1b.in', '1c.in']
+    files2 = ['2a.in', '2b.in']
+    files3 = ['3a.in', '3b.in', '3c.in', '3d.in']
+    
+    ## Parsing for part 1
+    for file in files1:
+        with open(inputdir + '/' + file) as fh:
+            data = fh.read().upper().split('\n')
+        for e in data:
+            if e == '': break                           # empty line
+            instr = e.split(' | ')
+            if file == '1b.in':                  # Dealing with 1b
+                res['1b'].append([instr[0], [int(e) for e in instr[1].split(' ')], instr[2]])
+            elif file == '1c.in':               # Dealing with 1c
+                res['1c'].append([instr[0], instr[1], instr[2]])
+            else:                                   # Dealing with 1a
+                res['1a'].append([instr[0], int(instr[1]), instr[2]])  
+    
+    return res;
 
-res = {'1a': {'ENCRYPT': [], 'DECRYPT': []}, '1b': {'ENCRYPT': [], 'DECRYPT': []}, '1c':{'ENCRYPT': [], 'DECRYPT': []}}
-
-inputdir = 'input'
-files1 = ['1a.in', '1b.in', '1c.in']
-files2 = ['2a.in', '2b.in']
-files3 = ['3a.in', '3b.in', '3c.in', '3d.in']
-
-## Parsing for part 1
-for file in files1:
-    with open(inputdir + '/' + file) as fh:
-        data = fh.read().upper().split('\n')
-    for e in data:
-        if e == '': break                           # empty line
-        instr = e.split(' | ')
-        if instr[1].find(' ') >= 0:                  # Dealing with 1b
-            if instr[0] == 'ENCRYPT':
-                res['1b']['ENCRYPT'].append([[int(e) for e in instr[1].split(' ')], instr[2]])
-            elif instr[0] == 'DECRYPT':
-                res['1b']['DECRYPT'].append([[int(e) for e in instr[1].split(' ')], instr[2]])
-        elif len(instr[1]) == 26:               # Dealing with 1c
-            if instr[0] == 'ENCRYPT':           # Should I process non-alpha out of here?
-                res['1c']['ENCRYPT'].append([instr[1], instr[2]])
-            elif instr[0] == 'DECRYPT':
-                res['1c']['DECRYPT'].append([instr[1], instr[2]])
-        else:                                   # Dealing with 1a
-            if instr[0] == 'ENCRYPT':
-                res['1a']['ENCRYPT'].append([int(instr[1]), instr[2]])
-            elif instr[0] == 'DECRYPT':
-                res['1a']['DECRYPT'].append([int(instr[1]), instr[2]])
-
-## Parse the big ass data file for part 2
-with open('ptb.train.txt') as fh:
-    rawtrain = fh.read();
-    train = re.sub('N |<unk> ', '', rawtrain)       # Remove all cases of N or <unk> followed by a space
-    train = re.sub(' N| <unk>', '', train)       # Remove all cases of N or <unk> preceeded by a space
-    train = re.sub('[^a-zA-Z ]', '', train)          # Remove all non-alpha characters, preserve spaces
-    train = train.upper()
-with open('dataset.txt', 'w') as fh:
-    fh.write(train)  
+def parse2(ds):
+    ## Parse the big data file for part 2
+    import re
+    with open(ds) as fh:
+        rawtrain = fh.read();
+        train = re.sub('N |<unk> ', '', rawtrain)       # Remove all cases of N or <unk> followed by a space
+        train = re.sub(' N| <unk>', '', train)          # Remove all cases of N or <unk> preceeded by a space
+        train = re.sub('[^a-zA-Z ]', '', train)         # Remove all non-alpha characters, preserve spaces
+        train = re.sub('  +', ' ', train)
+        train = train.upper()
+    with open('dataset.txt', 'w') as fh:
+        fh.write(train)
